af5d23e0e5011a49dd68baf5c24a9dcc259a035c
Vadym Kyrylenko <kyrylenko.vadym@gmail.com>
2018-03-01 18:51:16 +0200

    +Order

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ app.use(cors());
 const bodyParser = require('body-parser');
 require('./app_server/models/db');
 const routes = require('./app_server/routes/routes');
-const routesOrder = require('./app_server/routes/routesOrders')
+// const routesOrder = require('./app_server/routes/routesOrders');
 
-// const Product = require('./app_server/models/collectionsSchema').Product;
+
 let jsonParser = bodyParser.json();
 app.use(jsonParser);
 app.use('/', routes);
-app.use('/orders', routesOrder)
+// app.use('/orders', routesOrder);
 
 
 
--- app_server/routes/routes.js
+++ app_server/routes/routes.js
@@ -2,10 +2,19 @@ const express = require ('express');
 const router = express.Router();
 const productControllers = require('../controllers/productControllers');
 
+const orderControllers = require('../controllers/orderControllers');
+
 router.get('/products', productControllers.getProducts);
 router.post('/products', productControllers.postProducts);
 router.put('/products', productControllers.putProducts);
 router.delete('/products', productControllers.deleteProducts);
 router.get('/products/:idOfProduct', productControllers.getOneProduct);
+
+router.get('/orders', orderControllers.getOrders);
+router.post('/orders', orderControllers.postOrders);
+router.put('/orders', orderControllers.putOrders);
+router.delete('/orders', orderControllers.deleteOrders);
+router.get('/orders/:idOfOrder', orderControllers.getOneOrder);
+
 
 module.exports = router;
