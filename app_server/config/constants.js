09ade23eb11221900d96f82a4ab81a76ad789bda
Vadym Kyrylenko <kyrylenko.vadym@gmail.com>
2018-03-30 14:48:57 +0300

    mongoose options and mongo path was set in file constants.js

---
+++ app_server/config/constants.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const appConfig = {
+    // dbUrl: 'mongodb://18.218.221.213:27017/magaz',
+    dbUrl: 'mongodb://localhost:27017/magaz',
+    mongoose: {
+        promiseLibrary: global.Promise,
+        reconnectTries: Number.MAX_VALUE,
+        reconnectInterval: 1000,
+        native_parser: true
+    }
+};
+
+module.exports.APPCONST = appConfig;
